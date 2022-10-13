c894a879f7a8d900610f3e95fd7d0dd4461a76e4
Philip Kazakov <fliptv97@gmail.com>
2022-10-13 13:58:46 +0300

    Added helper for generating password

---
+++ src/helpers/generate-password.js
@@ -0,0 +1,32 @@
+const UPPERCASE_LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const LOWERCASE_LETTERS = 'abcdefghijklmnopqrstuvwxyz';
+const DIGITS = '0123456789';
+const SYMBOLS = '~`!@#$%^&*()_-+={[}]|\\:;"\'<,>.?/'
+
+function generatePassword(length, options = {}) {
+    let collection = '';
+
+    if (options?.uppercaseLetters) collection += UPPERCASE_LETTERS;
+    if (options?.lowercaseLetters) collection += LOWERCASE_LETTERS;
+    if (options?.numbers) collection += DIGITS;
+    if (options?.symbols) collection += SYMBOLS;
+
+    let result = '';
+
+    for (let i = 0; i < length; i++) {
+        result += collection[getRandomInt(0, collection.length)];
+    }
+
+    return result;
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    
+    // The maximum is exclusive and the minimum is inclusive
+    return Math.floor(Math.random() * (max - min) + min); 
+  }
+  
+
+export default generatePassword;
